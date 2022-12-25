c7489f4f6d1f529a1b07a04d98e8d3c73b3b7740
michaelOch <ochuenwikechukwudi@hotmail.com>
2022-12-25 19:03:27 +0100

    chore: update UI layout

---
+++ README.md
@@ -0,0 +1,5 @@
+## Rock Paper Scissors
+
+The popular game of Rock, Paper, Scissors.
+
+A game against a computer. The first to gain 5 points wins.
--- app.js
+++ app.js
@@ -1,52 +1,165 @@
+const containerFluid = document.querySelector('#container-fluid');
+containerFluid.setAttribute('style', 'display: flex; flex-direction: column; align-items: center; justify-content: center;');
+
+const round = document.querySelector('#round');
+round.textContent = 'Round: 0';
+
+const container = document.querySelector('#container');
+
+const vs = document.querySelector('#vs');
+const resetBtn = document.querySelector('#reset');
+const compContainer = document.querySelector('#comp-container');
+const playerchoice = document.querySelector('#playerchoice');
+const computerchoice = document.querySelector('#computerchoice');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const trophy = document.querySelector('#trophy');
+const sad = document.querySelector('#sad');
+const overlay = document.querySelector('.overlay');
+
+const optionStyle = 'width: 80px; padding: 10px 20px; cursor: pointer; color: #fff; margin-right: 1rem; border: none; border-radius: 100%;';
+
+//  Creating button elements with styling
+const btnRock = document.createElement('div');
+btnRock.setAttribute('key', 'Rock');
+btnRock.setAttribute('style', optionStyle);
+btnRock.style.backgroundColor = '#2f4f4f';
+btnRock.innerHTML = '<img class="w-100" src="./images/rock.png"  alt="rock" />';
+container.appendChild(btnRock);
+
+const btnPaper = document.createElement('div');
+btnPaper.setAttribute('key', 'Paper');
+btnPaper.setAttribute('style', optionStyle);
+btnPaper.style.backgroundColor = '#000080';
+btnPaper.innerHTML = '<img class="w-100" src="./images/paper.png"  alt="rock" />';
+container.appendChild(btnPaper);
+
+const btnScissors = document.createElement('div');
+btnScissors.setAttribute('key', 'Scissors');
+btnScissors.setAttribute('style', optionStyle);
+btnScissors.style.backgroundColor = '#8b4513';
+btnScissors.innerHTML = '<img class="w-100" src="./images/scissors.png"  alt="rock" />';
+container.appendChild(btnScissors);
+
+//  Adding event listeners to the buttons created
+btnRock.addEventListener('click', function(){
+    const playerSelection = btnRock.getAttribute('key');
+    playRound(playerSelection, computerPlay());
+});
+
+btnPaper.addEventListener('click', function(){
+    const playerSelection = btnPaper.getAttribute('key');
+    playRound(playerSelection, computerPlay());
+});
+
+btnScissors.addEventListener('click', function(){
+    const playerSelection = btnScissors.getAttribute('key');
+    playRound(playerSelection, computerPlay());
+});
+
+resetBtn.addEventListener('click', reset);
+
+const computerScore = document.querySelector('#computer-play h2');
+const playerScore = document.querySelector('#player-play h2');
+const finalScore = document.querySelector('#final-result');
+
+
 function computerPlay () {
     const select = Math.ceil(Math.random() * 3);
 
     if(select === 1) {
+        paper.classList.replace('show', 'hide');
+        scissors.classList.replace('show', 'hide');
+        rock.classList.replace('hide', 'show');
         return 'Rock';
     } else if (select === 2) {
+        rock.classList.replace('show', 'hide');
+        scissors.classList.replace('show', 'hide');
+        paper.classList.replace('hide', 'show');
         return 'Paper';
     } else {
+        paper.classList.replace('show', 'hide');
+        rock.classList.replace('show', 'hide');
+        scissors.classList.replace('hide', 'show');
         return 'Scissors';
     }
-}
-
-function playerPlay () {
-    let playerSelection = prompt("What's your play?\nNote:\n1. It must not be left empty.\n2. Choose either 'Rock'. 'Paper', 'Scissors'.\n3. It must not be a number.");
-    return playerSelection !== null && isNaN(playerSelection) ? playerSelection : playerPlay();
 }
 
 function playRound (playerSelection, computerSelection) {
     const playerSel = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     const computerSel = computerSelection;
 
-    console.log(`Player Selection: ${playerSel}`);
-    console.log(`Computer Selection: ${computerSel}`);
+    // console.log(`Player Selection: ${playerSel}`);
+    // console.log(`Computer Selection: ${computerSel}`);
 
-    let str;
+    playerchoice.textContent = playerSel;
+    computerchoice.textContent = computerSel;
+
+    finalScore.innerHTML = '';
+
     if((playerSel === 'Rock' && computerSel === 'Scissors') || (playerSel === 'Paper' && computerSel === 'Rock') || (playerSel === 'Scissors' && computerSel === 'Paper')) {
-        playerScore++;
-        str = `You Win! ${playerSel} beats ${computerSel}`;
-        return str;
-    } else if(playerSel === computerSel) {
-        str = `It's a tie!`;
-        return str;
+        playerScoreCount++;
+        playerScore.textContent = String(playerScoreCount);
+        vs.textContent = 'You win!';
+    } else if (playerSel === computerSel) {
+        vs.textContent = "It's a tie!";
     } else {
-        computerScore++;
-        str = `You Lose! ${computerSel} beats ${playerSel}`;
-        return str;
+        computerScoreCount++;
+        computerScore.textContent = String(computerScoreCount);
+        vs.textContent = 'You lose!';
+    }
+
+    if (playerScoreCount === 5 || computerScoreCount === 5) {
+        roundCount = 0;
+        round.textContent = `Round: ${String(roundCount)}`;
+    } else {
+        roundCount++;
+        round.textContent = `Round: ${String(roundCount)}`;
+    }
+
+    if(playerScoreCount === 5) {
+        vs.classList.replace('show', 'hide');
+        resetBtn.classList.replace('hide', 'show');
+        finalScore.classList.replace('hide', 'show');
+        finalScore.textContent = 'Congratulations!';
+        trophy.classList.replace('hide', 'show');
+        overlay.classList.replace('hide', 'show');
+        playerScoreCount = 0;
+        computerScoreCount = 0;
+    }
+
+    if(computerScoreCount === 5) {
+        vs.classList.replace('show', 'hide');
+        resetBtn.classList.replace('hide', 'show');
+        finalScore.classList.replace('hide', 'show');
+        finalScore.textContent = 'Oops. You Lost!!';
+        sad.classList.replace('hide', 'show');
+        overlay.classList.replace('hide', 'show');
+        playerScoreCount = 0;
+        computerScoreCount = 0;
     }
 }
 
-function game () {
-    for(let i = 1; i <= 5; i++) {
-        console.log(`Game Round: ${i}`);
-        console.log(playRound(playerPlay(), computerPlay()));
-    }
-
-    console.log(`Final Scoresheet:\nScore: Player ${playerScore} : ${computerScore} Computer`);
+function reset () {
+    playerScoreCount = 0;
+    playerScore.textContent = 0;
+    computerScoreCount = 0;
+    computerScore.textContent = 0;
+    playerchoice.textContent = '';
+    computerchoice.textContent = '';
+    vs.textContent = 'VS'
+    vs.classList.replace('hide', 'show');
+    resetBtn.classList.replace('show', 'hide');
+    finalScore.classList.replace('show', 'hide');
+    rock.classList.replace('show', 'hide');
+    scissors.classList.replace('show', 'hide');
+    paper.classList.replace('show', 'hide');
+    trophy.classList.replace('show', 'hide');
+    sad.classList.replace('show', 'hide');
+    overlay.classList.replace('show', 'hide');
 }
 
-let playerScore = 0;
-let computerScore = 0;
-
-game();
+let playerScoreCount = 0;
+let computerScoreCount = 0;
+let roundCount = 0;
